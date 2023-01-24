0e030c5c6e0c33a24a81bacbcbf82a9ce79ae0b8
Ankit Kumar <ankitkumar3624@gmail.com>
2023-01-25 00:16:01 +0530

    stripe checkout added

--- src/pages/success.js
+++ src/pages/success.js
@@ -21,7 +21,7 @@ const Success = () => {
             </h1>
           </div>
           <p>
-            Thank you for shopping with us. We'll send a confirmation once your
+            Thank you for shopping with us. We&apos;ll send a confirmation once your
             item has shipped, if you would like to check the status of your
             order(s) please press the link below.
           </p>
